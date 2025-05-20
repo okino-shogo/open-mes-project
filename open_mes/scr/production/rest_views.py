51b942483d3de62f98f9906b1157324ea798e754
yururi nonbiry <yururi.nonbiry@gmail.com>
2025-05-20 20:49:44 +0900

    生産計画のデータ読み込みまで。

--- open_mes/scr/production/rest_views.py
+++ open_mes/scr/production/rest_views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 # from rest_framework import permissions # Uncomment if you want to add permissions
 from .models import ProductionPlan, PartsUsed # PartsUsedモデルをインポート
 from .serializers import ProductionPlanSerializer, PartsUsedSerializer # PartsUsedSerializerをインポート
+from inventory.rest_views import StandardResultsSetPagination # inventoryアプリのページネーションクラスをインポート
 
 class ProductionPlanViewSet(viewsets.ModelViewSet):
     """
@@ -9,6 +10,7 @@ class ProductionPlanViewSet(viewsets.ModelViewSet):
     """
     queryset = ProductionPlan.objects.all().order_by('-planned_start_datetime')
     serializer_class = ProductionPlanSerializer
+    pagination_class = StandardResultsSetPagination # ページネーションクラスを指定
     # permission_classes = [permissions.IsAuthenticated] # Example: Add authentication
 
 class PartsUsedViewSet(viewsets.ModelViewSet):
@@ -17,4 +19,5 @@ class PartsUsedViewSet(viewsets.ModelViewSet):
     """
     queryset = PartsUsed.objects.all().order_by('-used_datetime')
     serializer_class = PartsUsedSerializer
+    pagination_class = StandardResultsSetPagination # ページネーションクラスを指定
     # permission_classes = [permissions.IsAuthenticated] # Example: Add authentication
